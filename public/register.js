cee5451fc7910e80c90b8d0e1af721cf1246b1b1
satthuhailua <satthuhailua@gmail.com>
2022-09-16 10:54:06 +0700

    sua loi 3

--- public/register.js
+++ public/register.js
@@ -1,4 +1,4 @@
 document.querySelector('.readyAccount').addEventListener('click',()=>{
     console.log('ready');
-    location = 'http://localhost:3000';
+    location = 'https://galaxychat.herokuapp.com/';
 })
