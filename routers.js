ac83bc3f97d22187705fed0ceb950d02638425a8
satthuhailua <satthuhailua@gmail.com>
2022-09-16 11:11:39 +0700

    sua loi 6

--- routers.js
+++ routers.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-router.get('https://galaxychat.herokuapp.com/',(req,res) =>{
+router.get('/',(req,res) =>{
     res.render('login');
 })
 module.exports = router;
